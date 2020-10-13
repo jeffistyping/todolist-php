610d59f9f76ef77ac681aa1de24e9a5850209717
Jeff Hui <jeffrey.kl.hui@gmail.com>
2020-10-13 14:08:39 -0400

    lamo

---
+++ tcp_server.c
@@ -0,0 +1,112 @@
+/* A simple echo server using TCP */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <strings.h>
+
+
+#define SERVER_TCP_PORT 3000	/* well-known port */
+#define BUFLEN		256	/* buffer length */
+
+int echod(int);
+void reaper(int);
+
+FILE *ifp;
+char *mode = "r";
+
+int main(int argc, char **argv)
+{
+	int 	sd, new_sd, client_len, port;
+	struct	sockaddr_in server, client;
+
+	switch(argc){
+	case 1:
+		port = SERVER_TCP_PORT;
+		break;
+	case 2:
+		port = atoi(argv[1]);
+		break;
+	case 3:
+		port = atoi(argv[1]);
+		ifp = fopen(argv[2], mode);
+	default:
+		fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Create a stream socket	*/	
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "Can't creat a socket\n");
+		exit(1);
+	}
+
+	/* Bind an address to the socket	*/
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+		fprintf(stderr, "Can't bind name to socket\n");
+		exit(1);
+	}
+
+	/* queue up to 5 connect requests  */
+	listen(sd, 5);
+
+	(void) signal(SIGCHLD, reaper);
+
+	while(1) {
+	  client_len = sizeof(client);
+	  new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
+	  if(new_sd < 0){
+	    fprintf(stderr, "Can't accept client \n");
+	    exit(1);
+	  }
+	  switch (fork()){
+	  case 0:		/* child */
+		(void) close(sd);
+		if (argc == 2){
+			exit(echod(new_sd));
+		}
+		else if (argc == 3) {
+			exit(fileshare(new_sd,ifp));
+		}
+	  default:		/* parent */
+		(void) close(new_sd);
+		break;
+	  case -1:
+		fprintf(stderr, "fork: error\n");
+	  }
+	}
+}
+
+int fileshare(int sd, FILE *file) 
+{	
+	fseek(file, 0L, SEEK_END); 
+    long int res = ftell(file); 
+	write(sd, "hellofile",9);
+	close(sd);
+	fclose(file); 
+	return(0);
+}
+
+/*	echod program	*/
+int echod(int sd)
+{
+	write(sd, "hello\n", 6);
+	close(sd);
+
+	return(0);
+}
+
+/*	reaper		*/
+void	reaper(int sig)
+{
+	int	status;
+	while(wait3(&status, WNOHANG, (struct rusage *)0) >= 0);
+}
